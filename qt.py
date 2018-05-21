d0b57311961902d78877c7d1090e176c90523f2f
alex <alex@experiencefellow.com>
2018-05-21 16:51:11 +0200

    cleanup

--- qt.py
+++ qt.py
@@ -388,7 +388,7 @@ if __name__ == '__main__':
 	while(True):
 		App = TetrisApp(show)
 		score, state, board_value = App.run(ai_agent)
-		score_str = "#" + str(i) + " ened with board value " + str(board_value) + " [Score: " + str(score) + "]"
+		score_str = "#" + str(ai_agent.games_played + i) + " ened with board value " + str(board_value) + " [Score: " + str(score) + "]"
 
 		if(show):
 			print(score_str)
