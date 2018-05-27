a1471ce61c869a18436e56f0b666584816f60d05
alex <alex@experiencefellow.com>
2018-05-27 17:21:13 +0200

    .

--- ai.py
+++ ai.py
@@ -46,6 +46,7 @@ class AI():
 			self.initialize_state(state_key)
 			actions = self.q_matrix[state_key]["actions"]
 
+		print("A: ", actions)
 		return actions
 
 		# if(state_key not in self.q_matrix.keys()):
