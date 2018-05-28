06b61d0e5fe6d460a6f5ba65ff0b86503c39c444
alex <alex@experiencefellow.com>
2018-05-28 19:29:39 +0200

    .

--- new_app.py
+++ new_app.py
@@ -7,45 +7,90 @@ import os
 import ai
 
 from pprint import pprint
+from time import sleep
 
-COLOR_EMPTY = 1
-COLOR_FRIENDLY = 2
-COLOR_FRIENDLY_ACTIVE = 3
-COLOR_ENEMY = 4
 
-EMPTY = 0
-UNIT_TYPE_ONE = 11
-UNIT_TYPE_TWO = 12
-UNIT_TYPE_THREE = 13
-UNIT_TYPE_FOUR = 14
+EMPTY = 1
+FRIENDLY = 2
+ENEMY = 3
+CONTESTED = 4
+FRIENDLY_UNIT = 5
+ENEMY_UNIT = 6
 
-FRIENDLY_UNIT_ONE= 21
-FRIENDLY_UNIT_TWO = 22
-FRIENDLY_UNIT_THREE = 23
-FRIENDLY_UNIT_FOUR = 24
+# 1 unit types
+UNIT_TYPE_ONE = 21
+UNIT_TYPE_TWO = 22
+UNIT_TYPE_THREE = 23
+UNIT_TYPE_FOUR = 24
 
-ENEMY_UNIT_ONE = 31
-ENEMY_UNIT_TWO = 32
-ENEMY_UNIT_THREE = 33
-ENEMY_UNIT_FOUR = 34
 
-UNIT_TYPES = [UNIT_TYPE_ONE, UNIT_TYPE_TWO, UNIT_TYPE_THREE, UNIT_TYPE_FOUR]
-FRIENDLY_UNITS = [FRIENDLY_UNIT_ONE, FRIENDLY_UNIT_TWO, FRIENDLY_UNIT_THREE, FRIENDLY_UNIT_FOUR]
-ENEMY_UNITS = [ENEMY_UNIT_ONE, ENEMY_UNIT_TWO, ENEMY_UNIT_THREE, ENEMY_UNIT_FOUR]
+UNIT_TYPES = [UNIT_TYPE_ONE] # , UNIT_TYPE_TWO, UNIT_TYPE_THREE, UNIT_TYPE_FOUR, UNIT_TYPE_ONE, UNIT_TYPE_TWO, UNIT_TYPE_TWO, UNIT_TYPE_TWO]
 
-UNIT_LABELS = { EMPTY: " ", ENEMY_UNIT_ONE: "A", ENEMY_UNIT_TWO: "B", ENEMY_UNIT_THREE: "C", ENEMY_UNIT_FOUR: "D",
-				FRIENDLY_UNIT_ONE: "A", FRIENDLY_UNIT_TWO: "B", FRIENDLY_UNIT_THREE: "C", FRIENDLY_UNIT_FOUR: "D" }
+
 class Unit(object):
 	def __init__(self, type, x, y):
 		self.type = type
 		self.x = x
 		self.y = y
+		self.last_pos = [self.x, self.y]
+
+	def move(self, dx, dy, rows, cols):
+		self.last_pos = [self.x, self.y]
+
+		new_pos = [self.x + dx, self.y + dy]
+
+		if(new_pos == self.last_pos):
+			return False
+
+		if(new_pos[0] < 0 or new_pos[0] >= cols or new_pos[1] < 0 or new_pos[1] > rows):
+			return False
+
+		self.x += dx
+		self.y += dy
+
+		return True
+
+	def circle(self, x0, y0, r):
+		x, y, p = 0, r, 1-r
+
+		L = []
+		L.append((x, y))
+
+		for x in range(int(r)):
+			if p < 0:
+				p = p + 2 * x + 3
+			else:
+				y -= 1
+				p = p + 2 * x + 3 - 2 * y
+
+			L.append((x, y))
+
+			if x >= y: break
+
+		N = L[:]
+		for i in L:
+			N.append((i[1], i[0]))
+
+		L = N[:]
+		for i in N:
+			L.append((-i[0], i[1]))
+			L.append((i[0], -i[1]))
+			L.append((-i[0], -i[1]))
+
+		N = []
+		for i in L:
+			N.append((x0+i[0], y0+i[1]))
+
+		return [[n[0], n[1] + 10] for n in N]
+
+	def expansion(self):
+		return [[self.x, self.y]] + self.circle(self.x, self.y, 3) # + self.circle(self.x, self.y , 2)
 
 class NewApp(object):
 	def __init__(self, show):
-		self.cell_size = 10
-		self.cols =	10
-		self.rows =	10
+		self.cell_size = 20
+		self.cols =	60
+		self.rows = 40
 		self.num_units = 6
 
 		pygame.init()
@@ -53,45 +98,90 @@ class NewApp(object):
 		if(show):
 			pygame.key.set_repeat(250,25)
 
-
 		self.show = show
 
-		curses.init_pair(COLOR_EMPTY, curses.COLOR_WHITE, curses.COLOR_WHITE)
-		curses.init_pair(COLOR_FRIENDLY, curses.COLOR_WHITE, curses.COLOR_BLUE)
-		curses.init_pair(COLOR_FRIENDLY_ACTIVE, curses.COLOR_CYAN, curses.COLOR_WHITE)
-		curses.init_pair(COLOR_ENEMY, curses.COLOR_WHITE, curses.COLOR_RED)
+		curses.init_pair(EMPTY, 231, 231)
+		curses.init_pair(FRIENDLY, 24, 24)
+		curses.init_pair(FRIENDLY_UNIT, 17, 17)
+		curses.init_pair(ENEMY_UNIT, 196, 196)
+		curses.init_pair(ENEMY, 203, 203)
 
+		curses.curs_set(0)
+
+		self.init_units()
 		self.init_board()
 
-	def init_board(self):
-		p = 1
-		self.board = [[EMPTY for j in range(self.cols)] for i in range(self.rows)]
-		a_x = self.cols - p
-		b_x = p
+	def init_units(self):
+		self.friendly_units = []
+		self.enemy_units = []
+
 
 		for i, unit_type in enumerate(UNIT_TYPES):
-			unit_a_x = a_x
-			unit_a_y = i + 1
-			unit_b_x = b_x
-			unit_b_y = i + 1
+			self.friendly_units.append(Unit(unit_type, 0, 0))
+			# self.enemy_units.append(Unit(unit_type, self.cols - 3, (5 * i) + 1))
 
-			self.board[unit_a_y][unit_a_x] = FRIENDLY_UNITS[i]
-			self.board[unit_b_y][unit_b_x] = ENEMY_UNITS[i]
+	def init_board(self):
+		self.board = [[EMPTY for j in range(self.cols)] for i in range(self.rows)]
+
+		for i, unit in enumerate(self.friendly_units):
+			for i, cell in enumerate(unit.expansion()):
+				x = cell[0]
+				y = cell[1] 
+
+				if(x > self.cols - 1 or x < 0 or y > self.rows - 1 or y < 0):
+					continue
+
+				if(self.board[y][x] == EMPTY):
+					self.board[y][x] = FRIENDLY
+				elif(self.board[y][x] == ENEMY):
+					self.board[y][x] = CONTESTED
+				else:
+					pass
+
+		for i, unit in enumerate(self.enemy_units):
+			for i, cell in enumerate(unit.expansion()):
+				x = cell[0]
+				y = cell[1]
+
+				if(x > self.cols - 1 or x < 0 or y >= self.rows - 1 or y < 0):
+					continue
+
+				try:
+					if(self.board[y][x] == EMPTY):
+						if(i == 0):
+							self.board[y][x] = ENEMY_UNIT
+						else:
+							self.board[y][x] = ENEMY
+					elif(self.board[y][x] == FRIENDLY):
+						self.board[y][x] = CONTESTED
+					else:
+						pass
+				except:
+					print(x)
+					print(y)
+					print(len(self.board))
+					print(len(self.board[0]))
+					raise Exception
 
 	def run(self, ai_agent, window):
 		while 1:
-			self.show_state(window)
+			for unit in self.friendly_units + self.enemy_units:
+				while not unit.move( np.random.randint(-1, 2),  np.random.randint(-1, 2), self.rows, self.cols): pass
+			
+				self.init_board()
+				self.show_state(window)
+				sleep(0.5)
 
 	def show_state(self, window):
+		fill = "   "
+
+		c = 3
+
 		for row_i, row in enumerate(self.board):
 			for col_i, e in enumerate(row):
-				if(e in FRIENDLY_UNITS):
-					cp = curses.color_pair(COLOR_FRIENDLY)
-				elif(e in ENEMY_UNITS):
-					cp = curses.color_pair(COLOR_ENEMY)
-				else:
-					cp = curses.color_pair(COLOR_EMPTY)
+				window.addstr(row_i, c * col_i, fill, curses.color_pair(e))
 
-				window.addstr(row_i, col_i, u"9632", cp)
+		for unit in self.friendly_units:
+			window.addstr(30, 0, str([unit.x, unit.y]))
 
 		window.refresh()
